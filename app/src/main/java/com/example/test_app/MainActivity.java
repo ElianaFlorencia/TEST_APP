abce06b1b0a62644ea01ab52d6b9dcf3c6600e8b
Eliana <eliana.cruz@davinci.edu.ar>
2023-10-20 21:11:36 -0300

    main activity con errores

--- app/src/main/java/com/example/test_app/MainActivity.java
+++ app/src/main/java/com/example/test_app/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.test_app;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.os.Bundle;
-import com.example.bottomnavigation.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
@@ -17,19 +18,20 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         replaceFragment(new HomeFragment());
-        binding.bottomNavigationView.setBackground(null);
+
+
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
             switch (item.getItemId()) {
                 case R.id.home:
                     replaceFragment(new HomeFragment());
                     break;
-                case R.id.shorts:
+                case R.id.collection:
                     replaceFragment(new CollectionFragment());
                     break;
-                case R.id.subscriptions:
+                case R.id.wishlist:
                     replaceFragment(new WishlistFragment());
                     break;
-                case R.id.library:
+                case R.id.profile:
                     replaceFragment(new ProfileFragment());
                     break;
             }
@@ -39,8 +41,9 @@ public class MainActivity extends AppCompatActivity {
     private void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment);
         fragmentTransaction.commit();
 
 
+}
 }
